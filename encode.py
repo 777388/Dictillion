7d7db6619d3c5707345fa4a53159326aec60f292
777388 <96343159+777388@users.noreply.github.com>
2023-08-11 12:08:56 -0700

    Add files via upload

---
+++ encode.py
@@ -0,0 +1,39 @@
+#
+#%00
+#%25         %30         %30
+#%25 %32 %35 %25 %33 %30 %25 %33 %30
+#↓↓↓
+#%25         %32         %35         %25         %33         %30         %25         %33         %30
+#%25 %32 %35 %25 %33 %32 %25 %33 %35 %25 %32 %35 %25 %33 %33 %25 %33 %30 %25 %32 %35 %25 %33 %33 %25 %33 %32
+
+
+
+#Recursive encoding
+
+import sys
+
+A = int(sys.argv[1])
+B = "%00"
+print(B)
+C = B
+D = 0
+E = ""
+while ( D <= A ):
+    for letter in C:
+        if letter == "%":
+            E += "%25"
+        if letter == "0":
+            E += "%30"
+        if letter == "2":
+            E += "%32"
+        if letter == "3":
+            E += "%33"
+        if letter == "5":
+            E += "%35"
+    D += 1
+    C = E
+    print(C)
+    E = ""
+    
+with open("encoded.txt", "w") as f:
+    f.write(C)
